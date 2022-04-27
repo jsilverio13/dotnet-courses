8965fe46a3667e2f4865101639c6535990aa81a3
Jefferson Silverio <jsilverio@ciandt.com>
2022-04-26 23:38:53 -0300

    Finalizando comunicação com aplicação e banco de dados via docker e kubernetes

---
+++ Microservices/PlatformService/Program.cs
@@ -0,0 +1,71 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PlatformService.AsyncDataServices;
+using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var services = builder.Services;
+var env = builder.Environment;
+bool isProd = env.IsProduction();
+
+
+if (isProd)
+{
+    Console.WriteLine("--> Using SqlServer Db");
+    services.AddDbContext<AppDbContext>(opt =>
+        opt.UseSqlServer(configuration.GetConnectionString("PlatformsConn")));
+}
+else
+{
+    Console.WriteLine("--> Using InMem Db");
+    services.AddDbContext<AppDbContext>(opt =>
+         opt.UseInMemoryDatabase("InMem"));
+}
+
+services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+services.AddScoped<IPlatformRepo, PlatformRepo>();
+services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+services.AddSingleton<IMessageBusClient, MessageBusClient>();
+services.AddControllers();
+services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
+});
+
+builder.Logging.AddConsole();
+
+Console.WriteLine($"--> CommandService Endpoint {configuration["CommandService"]}");
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseDeveloperExceptionPage();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlatformService v1"));
+
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    // endpoints.MapGrpcService<GrpcPlatformService>();
+
+    // endpoints.MapGet("/protos/platforms.proto", async context =>
+    // {
+    //     await context.Response.WriteAsync(File.ReadAllText("Protos/platforms.proto"));
+    // });
+
+});
+
+PrepDb.PrepPopulation(app, isProd);
+
+app.Run();
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dotnet-courses
+
+
+# Video
+https://www.youtube.com/watch?v=DgVjEo3OGBI&list=PLWJ_9UYCpxILyNgNniu9pHj5M3n29RZ8i&index=1&t=4543s&ab_channel=LesJackson
+
+# 6:06:02 PART 6 - MULTI-RESOURCE API
+https://www.youtube.com/watch?v=DgVjEo3OGBI&list=PLWJ_9UYCpxILyNgNniu9pHj5M3n29RZ8i&index=1&t=21962s
